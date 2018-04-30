cbf51dec40ca2b22ef6fe5e14ac65cacf98b1daf
zidi <zidi01@ukr.net>
2018-04-30 10:16:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,8 +3,7 @@ var playlist = {['Slowdive']: "Alison",
 }
 
 function updatePlaylist(playlist, artistName, songTitle) { // 
-  Object.assign({}, playlist, {['Phil Ochs']: "Here's to the State of Mississippi"})
-  return Object.assign({});
+  return Object.assign({}, playlist, {['Phil Ochs']: "Here's to the State of Mississippi"});
 }
 
 function removeFromPlaylist(playlist, artistName) { // 
